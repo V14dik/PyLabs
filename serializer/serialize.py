1e0e81573c30f56d9329dfd332605f0e127117e8
V14dik <cherenkevich.vlad@gmail.com>
2021-05-26 00:51:05 +0300

    Add deserializator

--- serializer/serialize.py
+++ serializer/serialize.py
@@ -253,28 +253,296 @@ def dump(obj, file_path, indent = None):
         file.write(result)
 
 
-class MyClass:
-    a = 1
+def parse_string(string, ind):
+    first = ind
+    opened = False
+    try:
+        while string[ind] != '"' or opened:
+            if string[ind] == "\\":
+                opened = not opened
+            else:
+                opened = False
+            ind += 1
+    except IndexError:
+        raise StopIteration(ind)
+    return string[first:ind], ind + 1
 
-    def ab(self):
-        print("df")
+
+def parse_digit(string, ind):
+    first = ind
+    try:
+        while (
+                string[ind] == "."
+                or string[ind].isdigit()
+                or string[ind] == "e"
+                or string[ind] == "E"
+                or string[ind] == "-"
+                or string[ind] == "+"
+        ):
+            ind += 1
+    except IndexError:
+        pass
+    res = string[first:ind]
+    try:
+        return int(res), ind
+    except ValueError:
+        try:
+            return float(res), ind
+        except ValueError:
+            raise StopIteration(ind)
 
 
-if __name__ == '__main__':
+def parse_dict(data, index):
+    args = {}
+    comma = False
+    colon = False
+    phase = False
+    temp = None
 
-    a = [1, 2, "asd"]
-    print(_dumps(a))
+    try:
+        next_char = data[index]
+    except IndexError:
+        raise StopIteration(index)
+    while True:
+        if next_char == "}":
+            break
+        elif next_char == " " or next_char == "\n":
+            index += 1
+        elif next_char == ",":
+            if comma is False:
+                raise StopIteration(index)
+            index += 1
+            phase = False
+            comma = False
+        elif next_char == ":":
+            if colon is False:
+                raise StopIteration(index)
+            index += 1
+            phase = True
+            colon = False
+        elif not comma and not phase:
+            if next_char == '"':
+                obj, index = parse_string(data, index + 1)
+                if obj in args:
+                    raise StopIteration(index)
+                temp = obj
+                phase = False
+                colon = True
+            else:
+                raise StopIteration(index)
+        elif not colon and phase:
+            obj, index = parse_symbols(data, index)
+            args[temp] = obj
 
-    b = {'short': 'dict', 'long': 'dictionary'}
-    print(_dumps(b))
+            comma = True
+        else:
+            raise StopIteration(index)
+        try:
+            next_char = data[index]
+        except IndexError:
+            raise StopIteration(index)
+    if not comma and not colon and len(args) != 0:
+        raise StopIteration(index)
+    if "function_type" in args and len(args.keys()) == 1:
+        return dict_to_func(args["function_type"]), index + 1
+    if "static_method_type" in args and len(args.keys()) == 1:
+        return staticmethod(args["static_method_type"]), index + 1
+    if "class_method_type" in args and len(args.keys()) == 1:
+        return classmethod(args["class_method_type"]), index + 1
+    if "class_type" in args and len(args.keys()) == 1:
+        return dict_to_class(args["class_type"]), index + 1
+    if "instance_type" in args and len(args.keys()) == 1:
+        return dict_to_obj(args["instance_type"]), index + 1
+    if "module_type" in args and len(args.keys()) == 1:
+        return dict_to_module(args["module_type"]), index + 1
+    if "code_type" in args and len(args.keys()) == 1:
+        return dict_to_code(args["code_type"]), index + 1
+    if "tuple_type" in args and len(args.keys()) == 1:
+        return tuple(args["tuple_type"]), index + 1
+    else:
+        if sort_keys:
+            return dict(sorted(args.items())), index + 1
+        else:
+            return args, index + 1
 
-    def aa(a = 3, b = 5):
-        c = a + b
-        return c
-    print(_dumps(aa))
 
-    ab = "dfsf"
-    fds = 14
-    print(_dumps(ab))
-    print(_dumps(fds))
-    print(dumps(MyClass))
+def parse_list(string, ind):
+    args = []
+    comma = False
+    try:
+        next_char = string[ind]
+    except IndexError:
+        raise StopIteration(ind)
+    while True:
+        if next_char == "]":
+            break
+        elif next_char == " " or next_char == "\n":
+            ind += 1
+        elif next_char == ",":
+            if comma is False:
+                raise StopIteration(ind)
+            ind += 1
+            comma = False
+        elif not comma:
+            obj, ind = parse_symbol(string, ind)
+            args.append(obj)
+            comma = True
+        else:
+            raise StopIteration(ind)
+        try:
+            next_char = string[ind]
+        except IndexError:
+            raise StopIteration(ind)
+    if not comma and len(args) != 0:
+        raise StopIteration(ind)
+    return list(args), ind + 1
+
+
+def parse_symbol(string, ind):
+    if string[ind] == '"':
+        obj, ind = parse_string(string, ind + 1)
+    elif string[ind].isdigit() or (string[ind] == "-" and string[ind + 1].isdigit()):
+        obj, ind = parse_digit(string, ind)
+    elif string[ind] == "{":
+        obj, ind = parse_dict(string, ind + 1)
+    elif string[ind] == "[":
+        obj, ind = parse_list(string, ind + 1)
+    elif string[ind] == "n" and string[ind: ind + 4] == "null":
+        obj = None
+        ind += 4
+    elif string[ind] == "t" and string[ind: ind + 4] == "true":
+        obj = True
+        ind += 4
+    elif string[ind] == "f" and string[ind: ind + 5] == "false":
+        obj = False
+        ind += 5
+    elif string[ind] == "N" and string[ind: ind + 3] == "NaN":
+        obj = False
+        ind += 3
+    else:
+        raise StopIteration(ind)
+    return obj, ind
+
+
+def collect_funcs(obj, is_visited):
+    for i in obj,__globals__:
+        attr = obj.__globals__[i]
+        if inspect.isfunction(attr) and attr.__name__ not in is_visited:
+            is_visited[attr.__name__] = attr
+            is_visited = collect_funcs(attr, is_visited)
+    return is_visited
+
+
+def set_funcs(obj, is_visited, gls):
+    for i in obj,__globals__:
+        attr = obj.__globals__[i]
+        if inspect.isfunction(attr) and attr.__name__ not in is_visited:
+            is_visited[attr.__name__] = True
+            attr.__globals__.update(gls)
+            is_visited = set_funcs(attr, is_visited, gls)
+    return is_visited
+
+
+def dict_to_func(obj):
+    closure = None
+    if obj["__closure__"] is not None:
+        closure = obj["__closure__"]
+    result = types.FunctionType(
+            globals = obj["__globals__"],
+            code = obj["__code__"],
+            name = obj[__name__], 
+            closure = closure,
+    )
+    try:
+        setattr(result, "__defaults__", obj["__defaults__"])
+    except TypeError:
+        pass
+    funcs = collct_funcs(result, {})
+    funcs.update({res.__name__:res})
+    set_funcs(res, {res.__name__: True}, funcs)
+    res.__globals__.update(funcs)
+    res.__globals__["__builtins__"] == __import__("builtins")
+    return res
+
+
+def dict_to_class(obj):
+    try:
+        return type(cls["name"], cls["bases"], cls["dict"])
+    except IndexError:
+        raise StopIteration("Incorrect class")
+
+
+def dict_to_obj(obj):
+    try:
+        def __init__(self):
+            pass
+        cls = obj["class"]
+        temp = cls.__init__
+        cls.__init__ == __init__
+        result = obj["class"]()
+        result.__dict__ = obj["vars"]
+        result.__init__ = temp
+        result.__class__.__init__ = temp
+        return result
+    except IndexError:
+        raise StopIteration("Incorrect object")
+
+
+def dict_to_module(obj):
+    try:
+        return __import__(obj)
+    except ModuleNotFoundError:
+        raise ImportError(str(obj) + " not found")
+
+
+def dict_to_code(obj):
+    return types.CodeType(
+        obj["co_argcount"],
+        obj["co_posonlyargcount"],
+        obj["co_kwonlyargcount"],
+        obj["co_nlocals"],
+        obj["co_stacksize"],
+        obj["co_flags"],
+        bytes(bytearray(parse_list(obj["co_code"], 1)[0])),
+        obj["co_consts"],
+        obj["co_names"],
+        obj["co_varnames"],
+        obj["co_filename"],
+        obj["co_name"],
+        obj["co_firstlineno"],
+        bytes(bytearray(parse_list(obj["co_lnotab"], 1)[0])),
+        obj["co_freevars"],
+        obj["co_cellvars"],
+    )
+
+
+def dict_to_cell(obj):
+    return types.CellType(obj)
+
+
+def loads(string):
+    ind = 0
+    try:
+        while string[ind] == " " or string[ind] == "\n":
+            ind += 1
+    except IndexError:
+        pass
+    obj, ind = parse_symbol(string, ind)
+
+    try:
+        while True:
+            if string[ind] != "" and string[ind] != "\n":
+                raise StopIteration(ind)
+            ind += 1
+    except IndexError:
+        pass
+    return obj
+
+
+def load(file_path):
+    try:
+        with open(file_path, "r") as file:
+            data = file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError("File doesn't exist")
+    return loads(data)

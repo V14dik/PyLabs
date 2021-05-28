35e8b330dfdb38135cd13c28d6c7d3489c3b2a2d
V14dik <cherenkevich.vlad@gmail.com>
2021-05-28 11:27:24 +0300

    Set up DB

--- TBot/workouts/admin.py
+++ TBot/workouts/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from workouts.models import Workout
 from workouts.models import Exercise
+from workouts.models import WorkoutExercises
 
 
 @admin.register(Workout)
 class WorkoutAdmin(admin.ModelAdmin):
-	list_display = ("name", "date", "time",)
+	list_display = ("name", "date", "time", "user")
 
 
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
 	list_display = ("name", )
+
+
+@admin.register(WorkoutExercises)
+class WorkoutExercises(admin.ModelAdmin):
+	list_display = ("workout", "exercises", "number",)
--- TBot/workouts/models.py
+++ TBot/workouts/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 import datetime
+from users.models import User
 
 
 class Workout(models.Model):
 	name  = models.CharField(max_length = 20)
 	date = models.DateField(default = datetime.date.today)
 	time = models.TimeField(default = datetime.time(9, 00))
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
 
 
 class Exercise(models.Model):
 	name = models.CharField(max_length = 20)
+	workouts = models.ManyToManyField(Workout, through = "WorkoutExercises")
 	
+
+class WorkoutExercises(models.Model):
+	workout = models.OneToOneField(Workout, on_delete = models.CASCADE)
+	number = models.IntegerField(default = 15)
+	exercises = models.ForeignKey(Exercise, on_delete = models.CASCADE)
